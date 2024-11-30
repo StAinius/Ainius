9a4cccac125784e7a0056163719549c771b9211d
Ainius Stonkus <stonkusainius@gmail.com>
2024-11-30 18:37:15 +0200

    Add files via upload

--- scripts/gallery-scripts.js
+++ scripts/gallery-scripts.js
@@ -1,5 +1,8 @@
 const translations = {
     "lt": {
+        "gallery-category-1": "Grafinis Dizainas",
+        "gallery-category-2": "Svetainės",
+        "gallery-category-3": "Interjero ir eksterjero dizainai",
         "home-nav": "Pagrindinis",
         "skills-nav": "Įgūdžiai",
         "services-nav": "Paslaugos",
@@ -7,6 +10,9 @@ const translations = {
         "contacts-nav": "Kontaktai"
     },
     "en": {
+        "gallery-category-1": "Graphic Design",
+        "gallery-category-2": "Websites",
+        "gallery-category-3": "Interior and Exterior Designs",
         "home-nav": "Home",
         "skills-nav": "Skills",
         "services-nav": "Services",
@@ -14,6 +20,9 @@ const translations = {
         "contacts-nav": "Contact"
     },
     "es": {
+        "gallery-category-1": "Diseño Gráfico",
+        "gallery-category-2": "Sitios Web",
+        "gallery-category-3": "Diseños de Interiores y Exteriores",
         "home-nav": "Inicio",
         "skills-nav": "Habilidades",
         "services-nav": "Servicios",
@@ -23,14 +32,17 @@ const translations = {
 };
 
 function changeLanguage(language) {
+    document.querySelector('.h1').textContent = translations[language]["header-title"];
+    document.querySelectorAll('.gallery-category')[0].querySelector('h2').textContent = translations[language]["gallery-category-1"];
+    document.querySelectorAll('.gallery-category')[1].querySelector('h2').textContent = translations[language]["gallery-category-2"];
+    document.querySelectorAll('.gallery-category')[2].querySelector('h2').textContent = translations[language]["gallery-category-3"];
 
     document.getElementById('home-nav').textContent = translations[language]["home-nav"];
     document.getElementById('skills-nav').textContent = translations[language]["skills-nav"];
     document.getElementById('services-nav').textContent = translations[language]["services-nav"];
     document.getElementById('gallery-nav').textContent = translations[language]["gallery-nav"];
     document.getElementById('contacts-nav').textContent = translations[language]["contacts-nav"];
-
- 
+    
     document.documentElement.lang = language;
 
     localStorage.setItem("preferredLanguage", language);
@@ -38,39 +50,12 @@ function changeLanguage(language) {
 
 document.addEventListener("DOMContentLoaded", () => {
     const preferredLanguage = localStorage.getItem("preferredLanguage") || "en";
-    changeLanguage(preferredLanguage); 
+    changeLanguage(preferredLanguage);
 });
-
 
 document.querySelectorAll('.flag-icon').forEach(img => {
     img.addEventListener('click', () => {
         const selectedLang = img.getAttribute('data-lang');
-        changeLanguage(selectedLang); 
+        changeLanguage(selectedLang);
     });
-});
-
-
-
-let currentIndex = 0;
-const images = document.querySelectorAll('.gallery-images .image-container');
-const totalImages = images.length;
-
-const prevButton = document.querySelector('.prev');
-const nextButton = document.querySelector('.next');
-
-function showImage(index) {
-    const offset = -index * 100;
-    images.forEach(image => {
-        image.style.transform = `translateX(${offset}%)`;
-    });
-}
-
-prevButton.addEventListener('click', () => {
-    currentIndex = (currentIndex === 0) ? totalImages - 1 : currentIndex - 1;
-    showImage(currentIndex);
-});
-
-nextButton.addEventListener('click', () => {
-    currentIndex = (currentIndex === totalImages - 1) ? 0 : currentIndex + 1;
-    showImage(currentIndex);
 });
